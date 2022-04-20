008986f947a579b70ad2533858f2e9ee1b3c3efa
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-20 19:58:21 +0430

    Design general idea of game

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
-#ifndef _RSDL_HH_
-#define _RSDL_HH_
-#include "src/rsdl.hpp"
 
+#include "src/rsdl.hpp"
+#include "game/game.hpp"
 int main(int argc, char *argv[])
 {
-    
+        Game game(4);
+    while(true)
+    {
+        game.render();
+        delay(100);
+    }
     return 0;
 }
-#endif
