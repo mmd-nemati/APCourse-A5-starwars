8404e0d0f9fd2746e0aff4fadf29af8033e49aa9
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-20 19:56:03 +0430

    Modify Spaceship class and Make .cpp

---
+++ game/player/spaceship.cpp
@@ -0,0 +1,2 @@
+#include "../../src/rsdl.hpp"
+class Spaceship {};
--- game/player/spaceship.hpp
+++ game/player/spaceship.hpp
@@ -1,5 +1,5 @@
 #ifndef __SPACESHIP_H__
-#define __SPACESHIP_H__
+#define __SPACESHIP_H__ "__SPACESHIP_H__"
 #include "../../src/rsdl.hpp"
 class Spaceship {};
 
