bfb8ff27c9b67e92d193b8b2ebae9d30a5345eda
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-22 03:36:53 +0430

    Draw spaceship body

--- game/game.cpp
+++ game/game.cpp
@@ -1,17 +1,19 @@
 #include "game.hpp"
 #include <iostream>
 Game::Game(int r)
+    : spaceship()
 {
     rounds = r;
     win = new Window(1024, 768);
     game_status = GAME_STOPPED;
-    // spaceship = Spaceship();
+    //spaceship = Spaceship(win);
 }
 
 void Game:: render()
 {
     win->clear();
     win->draw_img("assets/photos/back.png");
+    win->draw_rect(spaceship.get_body(), RED);
     win->update_screen();
     delay(50);
 }
--- game/game.hpp
+++ game/game.hpp
@@ -3,6 +3,8 @@
 #include "../src/rsdl.hpp"
 #include "player/spaceship.hpp"
 
+constexpr int WINDOW_WIDTH = 1024;
+constexpr int WINDOW_HEIGHT = 768;
 constexpr bool GAME_RUNNING = true;
 constexpr bool GAME_STOPPED = false;
 
