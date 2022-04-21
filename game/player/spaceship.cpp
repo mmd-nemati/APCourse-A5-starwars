651b8f07cff7ace87fde9d1146b2c76fe79317eb
Mmd <mohammad.r.nemati03@gmail.com>
2022-04-22 03:19:01 +0430

    Desing some attributes and constructor of Spaceship

--- game/player/spaceship.cpp
+++ game/player/spaceship.cpp
@@ -1,2 +1,19 @@
 #include "../../src/rsdl.hpp"
-class Spaceship {};
+#include "spaceship.hpp"
+
+
+Spaceship::Spaceship()
+  :  body(462, 668, 100, 100)
+{
+    // location =  random?;
+    vx = INITIAL_VELOCITY;
+    vy = INITIAL_VELOCITY;
+    ax = SPACESHIP_ACCELERATION;
+    ay = SPACESHIP_ACCELERATION;
+}
+
+///Spaceship::Spaceship()
+  ///: body(100, 100, 100, 100)
+///{
+ ///   return;
+///}
--- game/player/spaceship.hpp
+++ game/player/spaceship.hpp
@@ -1,6 +1,20 @@
 #ifndef __SPACESHIP_H__
-#define __SPACESHIP_H__ "__SPACESHIP_H__"
+#define __SPACESHIP_H__
 #include "../../src/rsdl.hpp"
-class Spaceship {};
+
+constexpr double INITIAL_VELOCITY = 0;
+constexpr double SPACESHIP_ACCELERATION = 0.1;
+class Spaceship 
+{
+    public:
+        Spaceship();
+        //Spaceship(Window* win);
+        Rectangle get_body() { return body; };
+    private:
+        Point location;
+        double vx, vy;
+        double ax, ay;
+        Rectangle body;
+};
 
 #endif
